d37e82e1e1ab6d9a91ad42bb95b2e275c0295288
pgm <740865629@qq.com>
2020-04-19 19:28:56 +0800

    更新ReadMe.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MiMusicBehavior
 自定义Behavior实现小米音乐歌手详情页
+## 效果预览
+![小米音乐歌手详情页效果](https://gitee.com/pgm250/blog_img_bed/raw/master/nestedScrolling/%E5%B0%8F%E7%B1%B3%E9%9F%B3%E4%B9%90%E6%AD%8C%E6%89%8B%E8%AF%A6%E6%83%85%E9%A1%B5.gif)
+apk下载地址：
+[github](https://raw.githubusercontent.com/pengguanming/MiMusicBehavior/master/app/release/app-release.apk)
+[百度云通道 密码:fu5s](https://pan.baidu.com/s/1nQVb0YDEjDBVZlSU2KK9ag)
--- app/src/main/java/com/pengguanming/mimusicbehavior/fragment/SongFragment.java
+++ app/src/main/java/com/pengguanming/mimusicbehavior/fragment/SongFragment.java
@@ -29,7 +29,7 @@ public class SongFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_food_layout, container, false);
+        View view = inflater.inflate(R.layout.fragment_song_layout, container, false);
         initView(view);
         initEvent();
         return view;
